a31280475b29e9bbb34be6e6264e555b6ae60be6
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-11-02 11:15:36 +0100

    add bonus part

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# 101-easy_print.py
+# Mohamed Kouchaoui <mohamed.kouchaoui19gmailcom>
+
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 103-fast_alphabet.py
+# Mohamed Kouchaoui <mohamed.kouchaoui19gmailcom>
+
+import string
+print(string.ascii_uppercase)
