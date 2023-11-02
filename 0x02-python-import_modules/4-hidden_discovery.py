bc3f2e39c5cd74fb3a188333a58a42ab0869d1b8
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-11-02 10:46:48 +0100

    exercices 2 to 5

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 4-hidden_discovery.py
+# Mohamed Kouchaoui <mohamed.kouchaoui19gmailcom>
+
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
