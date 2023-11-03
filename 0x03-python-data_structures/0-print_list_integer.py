35b0b0a9557973752212f99d570b48a6d6c26db5
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-11-03 10:03:29 +0100

    last commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 0-print_list_integer.py
+#Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
 def print_list_integer(my_list=[]):
         for i in my_list:
                 print("{:d}".format(i))
