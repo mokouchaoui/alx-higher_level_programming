abd4e08c20bcdf50d612423104e37aaf11bb41f5
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-11-07 12:43:59 +0100

    new files 0-rint

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,6 @@
 #Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
 
 def print_list_integer(my_list=[]):
-    for number in my_list:
-        print("{:d}".format(number))
+    """Prints all integers of a list"""
+    for i in range(len(my_list)):
+        print('{:d}'.format(my_list[i]))
