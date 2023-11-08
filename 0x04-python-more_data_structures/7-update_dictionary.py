78c11db3324c84cd9c54257f6ef3620397177324
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-11-08 08:14:35 -0800

    add new files from local repo to server

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
